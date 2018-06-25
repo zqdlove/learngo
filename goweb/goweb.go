7d6145d3152a27d4fe723f83130963376b1dbdee
张其栋 <18610807995@189.cn>
2018-06-25 14:32:20 +0800

    Add goweb

---
+++ goweb/goweb.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"net/http"
+	"fmt"
+	"log"
+	"sync"
+)
+
+var mu sync.Mutex
+var count int
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/count", handlercounter)
+	http.HandleFunc("/echo", handlerecho)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	count++
+	mu.Unlock()
+	fmt.Fprintf(w, "URL.PATH = %q\n", r.URL.Path)
+}
+func handlercounter(w http.ResponseWriter, r *http.Request){
+	mu.Lock()
+	fmt.Fprintf(w, "Count %d\n", count)
+	mu.Unlock()
+}
+
+func handlerecho(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "%s %s %s\n", r.Method, r.URL, r.Proto)
+	for k, v := range r.Header{
+		fmt.Fprintf(w, "Header[%q]= %q\n", k, v)
+	}
+	fmt.Fprintf(w, "Host = %q \n", r.Host)
+	fmt.Fprintf(w, "RemoteADDR = %q\n",r.RemoteAddr)
+	if err := r.ParseForm(); err != nil {
+		log.Print(err)
+	}
+	for k, v := range r.Form{
+		fmt.Fprintf(w, "Form[%q]=%q\n", k , v)
+	}
+}
